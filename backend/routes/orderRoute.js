e3a03d6518e84d2b2b0ed9e6aaaacd922b3aff79
soumboul fall <likadiop091955@gmail.com>
2025-05-31 18:15:05 +0100

    admin back

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -7,7 +7,7 @@ const orderRouter = express.Router();
 // admin features
 orderRouter.post("/list",adminAuth,allOrders)
 orderRouter.post("/status",adminAuth,updateStatus)
-orderRouter.post("/stats", adminAuth, getDashboardStats);
+orderRouter.get("/stats", adminAuth, getDashboardStats);
 
 
 // payments features
