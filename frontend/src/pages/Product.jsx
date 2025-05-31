e528aa802833ab7ff48e3bd4721553e5058000c6
soumboul fall <likadiop091955@gmail.com>
2025-05-31 13:26:04 +0100

    new reverse

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -61,7 +61,7 @@ const Product = () => {
             <p className='pl-2'>(122)</p>
 
           </div>
-          <p className='mt-5 text-3xl font-medium'>{currency}{productData.price}</p>
+          <p className='mt-5 text-3xl font-medium'>{productData.price}{currency}</p>
           <p className='mt-5 text-gray-500 md:w-4/5'>{productData.description}</p>
           <div className='flex flex-col gap-4 my-8'>
             <p>Select size</p>
