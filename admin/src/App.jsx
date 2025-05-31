683b13a6bb7ce13c8c3b60aaecf1fbe3ea720389
soumboul fall <likadiop091955@gmail.com>
2025-05-31 17:39:43 +0100

    admin stats

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,6 +5,7 @@ import { Routes, Route } from 'react-router-dom'
 import Add from './pages/Add'
 import List from './pages/List'
 import Orders from './pages/Orders'
+import DashboardStats from "./pages/DashboardStats"
 import Login from './components/Login'
 import { ToastContainer } from 'react-toastify'
 import "react-toastify/dist/ReactToastify.css"
@@ -37,6 +38,7 @@ const App = () => {
                 <Route path='/add' element={<Add token={token} />} />
                 <Route path='/list' element={<List token={token} />} />
                 <Route path='/orders' element={<Orders token={token} />} />
+                <Route path='/stats' element={<DashboardStats token={token} />} />
               </Routes>
             </div>
           </div>
